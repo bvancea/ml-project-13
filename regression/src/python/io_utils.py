513a3824e655a6a5b52e42353c44bed3a54274ab
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2013-10-23 22:11:03 +0200

    #1: Added polynomial regressions and updated input parsing to use pandas.

--- regression/src/python/io_utils.py
+++ regression/src/python/io_utils.py
@@ -1,21 +1,61 @@
 import numpy as np
+import pandas as pd
+
 from numpy import genfromtxt
 
 
-def read_regression_data(training_file=None,testing_file=None,validation_file=None, delimiter=None):
+TRAINING_IN = "../resources/training.csv"
+VALIDATION_IN = "../resources/validation.csv"
+TESTING_IN = "../resources/validation.csv"
+TESTING_OUT = './out/testing_y.out'
+VALIDATION_OUT = './out/validation_y.out'
+
+
+#some relevant column names
+headers = ['width','rob','iq','lsq','rfsize','rfread','rfwrite','gshare','btb','branches','l1icache','l1dcache','l2ucache','depth','delay']
+
+
+def preprocess_features(feature_vector, relevant_features):
+
+    poly_order = 5
+    features_x = np.vander(feature_vector[relevant_features[0]], poly_order)
+    for name in relevant_features[1:]:
+        current_x = np.vander(feature_vector[name], poly_order)
+        features_x = np.concatenate((current_x, features_x), axis=1)
+
+    return features_x
+
+
+def read_x(filename=None, header_names=None):
+    if header_names is None:
+        header_names = headers[:14]
+
+    return pd.read_csv(filename, header=None, names=header_names)
+
+
+def read_x_y(filename=None, header_names=None):
+    if header_names is None:
+        header_names = headers
+
+    x_y = pd.read_csv(filename, header=None, names=header_names)
+
+    return x_y
+
+
+def read_regression_data(training_file=None, testing_file=None, validation_file=None, delimiter=None):
     if delimiter is None:
         delimiter = ','
     if training_file is None:
         training_file = '../resources/training.csv'
 
-    training_ret = genfromtxt(training_file, delimiter=delimiter, usecols=range(13))
+    training_ret = genfromtxt(training_file, delimiter=delimiter, usecols=range(14))
     training_label = genfromtxt(training_file, delimiter=delimiter, usecols=14)
     if validation_file is None:
         validation_file = '../resources/validation.csv'
-    validation_ret = genfromtxt(validation_file, delimiter=delimiter, usecols=range(13))
+    validation_ret = genfromtxt(validation_file, delimiter=delimiter, usecols=range(14))
     if testing_file is None:
         testing_file = '../resources/testing.csv'
-    testing_ret = genfromtxt(testing_file, delimiter=delimiter, usecols=range(13))
+    testing_ret = genfromtxt(testing_file, delimiter=delimiter, usecols=range(14))
     return training_ret, training_label, testing_ret, validation_ret
 
 
--- regression/src/python/regression.py
+++ regression/src/python/regression.py
@@ -1,45 +1,44 @@
 __author__ = 'bogdan'
 
-import io_utils as io
-import numpy as np
 from sklearn import linear_model
-from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
+import pandas as pd
+import io_utils as io
 
-TRAINING_IN = "../resources/training.csv"
-VALIDATION_IN = "../resources/validation.csv"
-TESTING_IN = "../resources/validation.csv"
 
-TESTING_OUT = './out/testing_y.out'
-VALIDATION_OUT = './out/validation_y.out'
-
+relevant_features = ['width', 'rob', 'iq', 'lsq', 'l2ucache', 'depth']
 
 #read input data
-training_X, training_y, testing_X, validation_X = io.read_regression_data()
+#training_X, training_y, testing_X, validation_X = io.read_regression_data()
 
-#Scale input data with respect to mean and std dev
-scaler = StandardScaler()
-scaler.fit(training_X)
-training_X = scaler.transform(training_X)
-testing_X = scaler.transform(testing_X)
-validation_X = scaler.transform(validation_X)
+training = io.read_x_y(io.TRAINING_IN)
+training_y = training.delay
+testing_X = io.read_x(io.TESTING_IN)
+validation_X = io.read_x(io.VALIDATION_IN)
 
-rng = np.random.RandomState(1)
+#scale the data
+#scaler = StandardScaler()
+#scaler.fit(training_X)
+#training_X = scaler.transform(training_X)
+#testing_X = scaler.transform(testing_X)
+#validation_X = scaler.transform(validation_X)
+pd.scatter_matrix(training,alpha=0.5,figsize=(10,10), diagonal='hist')
+
+#preprocess training
+training_X = io.preprocess_features(training, relevant_features)
+validation_X = io.preprocess_features(validation_X, relevant_features)
+
 #train regressor
-#regularizers = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
-#regressor = linear_model.RidgeCV(regularizers, normalize=True)
-regressor = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
-                          n_estimators=300, random_state=rng)
+#kf = KFold(len(training_X), n_folds=10)
+regularizers = [0.0001, 0.001, 0.05, 0.001, 0.05, 0.01, 0.1, 1.0, 5, 10, 100]
 
+regressor = linear_model.Ridge()
 regressor.fit(training_X, training_y)
 
 
 #make predictions
-testing_y = regressor.predict(testing_X)
 validation_y = regressor.predict(validation_X)
 
 #write to output file
-io.write_prediction(TESTING_OUT, testing_y)
-io.write_prediction(VALIDATION_OUT, validation_y)
+#io.write_prediction(TESTING_OUT, testing_y)
+io.write_prediction(io.VALIDATION_OUT, validation_y)
 
